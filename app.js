4405b77a6d52243b45c42aed37e2b8ba31b2fd6e
Illia Komissarov <iliakomissarow99@gmail.com>
2023-09-03 13:25:26 +0300

    new code

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-const express = require("express");
-const logger = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
-const contactsRouter = require("./routes/api/contacts");
-const app = express();
-const formatsLogger = app.get("env") === "development" ? "dev" : "short";
+import express, { json } from 'express'
+import logger from 'morgan'
+import cors from 'cors'
+import dotenv from 'dotenv';
+dotenv.config();
 
-app.use(logger(formatsLogger));
-app.use(cors());
-app.use(express.json());
-app.use(express.static("public"));
+import authRouter from './routes/api/authRouter.js';
+import contactsRouter from './routes/api/contactsRouter.js'
 
-app.use("/api/contacts", contactsRouter);
+const app = express()
 
-app.use((_, res) => {
-  res.status(404).json({ message: "Not found" });
-});
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.use((err, _, res, __) => {
-  const { status = 500, message = "Server error. Please try later" } = err;
-  res.status(status).json({ message });
-});
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(json())
 
-module.exports = app;
+app.use('/api/users', authRouter);
+app.use('/api/contacts', contactsRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
+})
+
+app.use((err, req, res, next) => {
+  const { status = 500, message = 'Server error' } = err;
+  res.status(status).json({ message })
+})
+
+export default app
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const mongoose = require("mongoose");
+import app from './app.js'
+import mongoose from 'mongoose'
 
-const app = require("./app");
-const { DB_HOST, PORT = 3000 } = process.env;
+// app.listen(3000, () => {
+//   console.log("Server running. Use our API on port: 3000")
+// })
 
-mongoose.set("strictQuery", true);
+const { DB_HOST } = process.env;
+const PORT = process.env.PORT || 3000;
 
-mongoose
-  .connect(DB_HOST)
-  .then(() =>
-    app.listen(PORT, () => {
-      console.log("Server running. Use our API on port: 3000");
-    })
-  )
-  .catch((error) => {
+mongoose.connect(DB_HOST)
+  .then(() => {
+    app.listen(PORT);
+    console.log('Database connection successful!');
+  })
+  .catch(error => {
     console.log(error.message);
     process.exit(1);
   });
