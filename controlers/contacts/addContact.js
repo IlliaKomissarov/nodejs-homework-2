1004fbd324cc1bd8a32869b5888f64c5b8514b1d
Illia Komissarov <iliakomissarow99@gmail.com>
2023-09-03 18:18:17 +0300

    Update addContact.js

--- controlers/contacts/addContact.js
+++ controlers/contacts/addContact.js
@@ -1,19 +1,23 @@
-import { Contact } from '../../schemas/contacts.js';
+import { Contact } from "../../schemas/contacts.js";
 import { HttpError } from "../../helpers/HttpErrors.js";
-import { ctrlWrapper } from "../../helpers/ctrlWraper.js";
+import { ctrlWrapper } from "../../helpers/ctrlWrapper.js";
 
 const add = async (req, res) => {
   const { name, email, phone } = req.body;
-    const { error } = req.body;
-    if (error) {
-      throw HttpError(400, `Missing required name field`)
-    }
-    const result = await Contact.create({ name, email, phone})
-    res.status(201).json({
-      status: 'success',
-      code: 201,
-      data: { contact: result },
-    })
-}
+  const { _id: owner } = req.user;
 
-export const addContact = ctrlWrapper(add)
+  if (!name) {
+    throw HttpError(400, `Missing required name field`);
+  }
+
+  const result = await Contact.create({ name, email, phone, owner });
+  контакту;
+
+  res.status(201).json({
+    status: "success",
+    code: 201,
+    data: { contact: result },
+  });
+};
+
+export const addContact = ctrlWrapper(add);
